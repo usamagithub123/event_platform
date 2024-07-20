ccf4e58dc532419299a33ae2617443f5becbcda6
usamagithub123 <usamaashraf540@gmail.com>
2024-07-20 21:21:13 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+    publicRoutes:[
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+
+
+    ],
+    ignoredRoutes:[
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+    ]
+}
+);
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
