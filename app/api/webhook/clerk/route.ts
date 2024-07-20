6a9b6563a29f1f3459ac8c0b1c1559c42e62f723
usamagithub123 <usamaashraf540@gmail.com>
2024-07-20 21:33:37 +0530

    second commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,6 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
